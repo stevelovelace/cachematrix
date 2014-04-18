1778ab592ff1bdce79aa4293bee789b71b1372ed
Steve Lovelace <steve@lovelace.net>
2014-04-17 18:26:23 -0700

    bug fix, more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,8 @@
 ## A facility for caching the inverse of a matrix
+    ## the following was a big help understanding scoping:
+    ##   http://www.r-bloggers.com/environments-in-r/
+    ## as well as the original Ihaka/Gentleman paper, which I found here:
+    ##   http://www.tandfonline.com/doi/pdf/10.1080/10618600.1996.10474713
 
 ## makeCacheMatrix() takes a matrix as its parameter and returns
 ## a List of functions for manipulating the matrix and its
@@ -22,18 +26,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## cacheSolve() takes a matrix cache (produced by makeCacheMatrix()) and
-## returns returns either a cached inverse or a newly calculated one
+## returns either a cached inverse or a newly calculated one
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x' 
-    inv <- cache$getinv()
+    inv <- x$getinv()
     if (!is.null(inv)) {
         message("getting cached inverse")
         return(inv)
     }
     message("calculating inverse")
-    mat <- cache$get()
+    mat <- x$get()
     inv <- solve(mat, ...)
-    cache$setinv(inv)
+    x$setinv(inv)
     inv
 }
